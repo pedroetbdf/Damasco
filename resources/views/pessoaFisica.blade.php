d7b18ab8ac2d3b53e1ca762dc6e2595ca234f146
pedrosouza <pedroetbdf@gmail.com>
2018-10-10 14:15:16 -0300

    componentes

--- resources/views/pessoaFisica.blade.php
+++ resources/views/pessoaFisica.blade.php
@@ -3,15 +3,34 @@
 @section('title', 'AdminLTE')
 
 @section('content_header')
-    <h1>Dashboardsadf fatte</h1>
+    <h1>Cadastro de Pessoa Física</h1>
 @stop
 
 @section('content')
-    wer
+    <!-- Styles -->
+    <link href="{{ asset('vendor/adminlte/bower_components/bootstrap/dist/css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminlte/bower_components/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminlte/bower_components/Ionicons/css/ionicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminlte/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminlte/plugins/iCheck/all.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminlte/bower_components/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminlte/plugins/timepicker/bootstrap-timepicker.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminlte/bower_components/select2/dist/css/select2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminlte/bower_components/select2/dist/css/select2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/adminlte/dist/css/skins/_all-skins.min.css') }}" rel="stylesheet">
+
+
+
+
+
+
+
+
     <!-- Cadastrar Pessoa Form -->
     <div class="box box-info">
         <div class="box-header with-border">
-            <h3 class="box-title">Horizontal Form</h3>
+            <h3 class="box-title"></h3>
         </div>
         <!-- /.box-header -->
         <!-- form start -->
@@ -25,20 +44,50 @@
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="campoDataNascimento" class="col-sm-2 control-label">Data de nascimento</label>
+                    <label for="campoDataNascimento1" class="col-sm-2 control-label">CPF</label>
 
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" id="campoDataNascimento" placeholder="Password">
+                        <input type="password" class="form-control" id="campoDataNascimento1" placeholder="Password">
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <label for="campoidade" class="col-sm-2 control-label">Data de nascimento</label>
+                    <label for="campoDataNascimento2" class="col-sm-2 control-label">RG</label>
 
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" id="campoidade" placeholder="Password">
+                        <input type="password" class="form-control" id="campoDataNascimento2" placeholder="Password">
                     </div>
                 </div>
+
+                <div class="form-group">
+                    <label for="campoDataNascimento3" class="col-sm-2 control-label">UF Nascimento</label>
+
+                    <div class="col-sm-10">
+                        <select class="form-control" id="campoDataNascimento3" >
+                            <option>Selecione</option>
+                            <option>Ceará</option>
+                            <option>Distrito Federal</option>
+                            <option>Bahia</option>
+                            <option>option 5</option>
+                        </select>
+
+                    </div>
+                </div>
+
+
+
+                <div class="form-group">
+                    <label class="col-md-2 control-label" for="Date">Data de Nascimento</label>
+                    <div class="col-md-10">
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <i class="fa fa-calendar"></i>
+                            </div>
+                            <input type="text" class="form-control pull-right" id="datepicker"/>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="form-group">
                     <div class="col-sm-offset-2 col-sm-10">
                         <div class="checkbox">
@@ -49,6 +98,8 @@
                     </div>
                 </div>
             </div>
+
+
             <!-- /.box-body -->
             <div class="box-footer">
                 <button type="submit" class="btn btn-default">Cancel</button>
@@ -57,4 +108,67 @@
             <!-- /.box-footer -->
         </form>
     </div>
+
+
+    <!-- Scripts -->
+    <script src="{{ asset('vendor/adminlte/bower_components/jquery/dist/jquery.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/bower_components/bootstrap/dist/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/bower_components/select2/dist/js/select2.full.min.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/plugins/input-mask/jquery.inputmask.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/plugins/input-mask/jquery.inputmask.date.extensions.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/plugins/input-mask/jquery.inputmask.extensions.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/bower_components/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+
+    {{--<script src="{{ asset('vendor/adminlte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>--}}
+    {{--<script src="{{ asset('vendor/adminlte/bower_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js') }}"></script>--}}
+    {{--<script src="{{ asset('vendor/adminlte/plugins/timepicker/bootstrap-timepicker.min.js') }}"></script>--}}
+    {{--<script src="{{ asset('vendor/adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>--}}
+    {{--<script src="{{ asset('vendor/adminlte/plugins/timepicker/bootstrap-timepicker.min.js') }}"></script>--}}
+
+    {{--<link rel="stylesheet" type="text/css" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />--}}
+    {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">--}}
+    {{--<link rel="stylesheet" href="https://cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" />--}}
+    {{--<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>--}}
+    {{--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>--}}
+    {{--<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>--}}
+    {{--<script type="text/javascript" src="https://cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>--}}
+
+    <script src="{{ asset('vendor/adminlte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js') }}"></script>
+    <script src="{{ asset('vendor/adminlte/bower_components/bootstrap-datepicker/js/bootstrap-datepicker.pt-BR.js') }}"></script>
+
+    <script type="text/javascript">
+
+
+        var versaoJquery = $.fn.jquery;
+        console.log('Versão jQuery: ' + versaoJquery);
+        //Date picker
+        //Date picker
+
+
+
+        $('#datepicker').datepicker({
+            format: 'dd/mm/yyyy',
+            language: 'pt-BR',
+            autoclose:true
+
+        });
+        // $('#datepicker').on('changeDate', function(ev){
+        //     $(this).datepicker('hide');
+        // });
+            // $('#datepicker').datepicker({
+            //     defaultDate: "11/1/2013",
+            //     locale: 'pt-br'
+            // });
+
+
+        $( document ).ready(function() {
+
+            console.log( "ready!" );
+
+        });
+
+
+
+    </script>
 @stop
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -301,6 +301,7 @@ class ComposerStaticInit9aff29d4a438e979d7a980d17f8e7656
         'App\\Http\\Controllers\\Auth\\ResetPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ResetPasswordController.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
         'App\\Http\\Controllers\\HomeController' => __DIR__ . '/../..' . '/app/Http/Controllers/HomeController.php',
+        'App\\Http\\Controllers\\PessoaFisicaController' => __DIR__ . '/../..' . '/app/Http/Controllers/PessoaFisicaController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
         'App\\Http\\Middleware\\RedirectIfAuthenticated' => __DIR__ . '/../..' . '/app/Http/Middleware/RedirectIfAuthenticated.php',
